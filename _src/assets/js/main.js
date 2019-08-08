cd52607e2ac1b41e71ab6b115640990fdf086dc8
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-08-08 09:45:29 +0200

    add fetch to search on Api

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -5,11 +5,28 @@ const searchInput = document.querySelector('.search__input');
 const searchButton = document.querySelector('.search__button');
 const resultList = document.querySelector('.results__list');
 const ListElement = document.querySelector('.result__list__element');
+const myApi = `http://api.tvmaze.com/search/shows?q=${searchInput.value}`;
+
+
 
 //local storage
 
 //funciones
+function handleButtonClick () {
+  fetch(myapi)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      for (let i=0; i<data.length; i++) {
+
+      }
 
 
+
+    });
+}
 
 //listeners
+
+searchButton.addEventListener('click', handleButtonClick);
