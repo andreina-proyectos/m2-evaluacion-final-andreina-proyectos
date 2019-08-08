46326d556603e375e41c831a58e7ad7885fb0414
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-08-08 12:59:34 +0200

    add correct conditional to image default and image medium, now it works

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -5,22 +5,21 @@ const searchInput = document.querySelector('.search__input');
 const searchButton = document.querySelector('.search__button');
 const resultList = document.querySelector('.results__list');
 const ListElement = document.querySelector('.result__list__element');
-const defaultImage = 'https://img.icons8.com/cotton/2x/cinema-.png';
-
+const defaultImage = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT83m54gAolcMA_rV6DRKCNfP0r_M_dxZ1BBVeVJ6c-StaEz07w';
 
 
 //local storage
 
 //funciones
-function handleInputSearch() {
-  const userValue = searchInput.value;
-  if (userValue) {
-    localStorage.setItem('Searched value', userValue)
-  }
-  else {
-    localStorage.removeItem('Searched value');
-  }
-}
+// function handleInputSearch() {
+//   const userValue = searchInput.value;
+//   if (userValue) {
+//     localStorage.setItem('Searched value', userValue)
+//   }
+//   else {
+//     localStorage.removeItem('Searched value');
+//   }
+// }
 
 function handleButtonClick () {
   const searchedValue = searchInput.value;
@@ -33,28 +32,27 @@ function handleButtonClick () {
       resultList.innerHTML = '';
       for (let i=0; i<data.length; i++) {
         const myObject = data[i];
-        const myResultImage = '';
-        const myResultName = myObject.show.name;
-        const myLiResult =
-          `<li class="result__list__element">
-          <img src="${myResultImage.medium}" alt="" class="result__image">
-          <h2 class="result__name">${myResultName}</h2>
-          </li>`;
+        let myResultImage;
+        let myResultName = myObject.show.name;
 
-
-        if (myResultImage === null) {
-          myResultImage.src = defaultImage;
-          resultList.innerHTML += myLiResult;
+        if (myObject.show.image === null) {
+          myResultImage = defaultImage;
         }
 
         else {
-          myResultImage.src = myResultImage.medium;
-          resultList.innerHTML += myLiResult;
+          myResultImage = myObject.show.image.medium;
         }
+
+        resultList.innerHTML +=
+        `<li class="result__list__element">
+        <img src="${myResultImage}" alt="" class="result__image">
+        <h2 class="result__name">${myResultName}</h2>
+        </li>`;
       }
     });
 }
 
 //listeners
 searchButton.addEventListener('click', handleButtonClick);
-searchInput.addEventListener('keyup', handleInputSearch);
+
+//searchInput.addEventListener('keyup', handleInputSearch)
