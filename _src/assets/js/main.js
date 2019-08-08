52181e94e0178783ea40f78cb1df625ca9f746b9
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-08-08 11:33:38 +0200

    add conditionals to image default and medium image

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -5,6 +5,7 @@ const searchInput = document.querySelector('.search__input');
 const searchButton = document.querySelector('.search__button');
 const resultList = document.querySelector('.results__list');
 const ListElement = document.querySelector('.result__list__element');
+const defaultImage = 'https://img.icons8.com/cotton/2x/cinema-.png';
 
 
 
@@ -29,15 +30,27 @@ function handleButtonClick () {
       return response.json();
     })
     .then(function(data) {
-      //resultList.innerHTML = '';
+      resultList.innerHTML = '';
       for (let i=0; i<data.length; i++) {
-        const  myObject = data[i];
+        const myObject = data[i];
+        const myResultImage = '';
+        const myResultName = myObject.show.name;
         const myLiResult =
-        `<li class="result__list__element">
-        <img src="${myObject.show.image.medium}" alt="" class="result__image">
-        <h2 class="result__name">${myObject.show.name}</h2>
-        </li>`
-      resultList.innerHTML = resultList.innerHTML + myLiResult;
+          `<li class="result__list__element">
+          <img src="${myResultImage.medium}" alt="" class="result__image">
+          <h2 class="result__name">${myResultName}</h2>
+          </li>`;
+
+
+        if (myResultImage === null) {
+          myResultImage.src = defaultImage;
+          resultList.innerHTML += myLiResult;
+        }
+
+        else {
+          myResultImage.src = myResultImage.medium;
+          resultList.innerHTML += myLiResult;
+        }
       }
     });
 }
@@ -45,4 +58,3 @@ function handleButtonClick () {
 //listeners
 searchButton.addEventListener('click', handleButtonClick);
 searchInput.addEventListener('keyup', handleInputSearch);
-
