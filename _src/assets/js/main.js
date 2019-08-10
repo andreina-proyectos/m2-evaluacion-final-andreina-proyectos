4d9700e991344049b1a501f8fc30a730e18f9a13
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-08-10 21:03:43 +0200

    delete comments on main.js and improve styles

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -9,6 +9,7 @@ const myFavoriteList = document.querySelector('.favorites__list');
 const defaultImage = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT83m54gAolcMA_rV6DRKCNfP0r_M_dxZ1BBVeVJ6c-StaEz07w';
 let arrFavorites = [];
 
+
 //local storage
 const lsUserFavoriteShow = localStorage.getItem('user-favorites-shows');
 if (lsUserFavoriteShow) {
@@ -17,7 +18,6 @@ if (lsUserFavoriteShow) {
 }
 
 //funciones
-
 function handleButtonClick () {
   const searchedValue = searchInput.value;
   const myApi = `http://api.tvmaze.com/search/shows?q=${searchedValue}`;
@@ -42,55 +42,47 @@ function handleButtonClick () {
         else {
           myResultImage = myObject.show.image.medium;
         }
-        //ejecuto función fabricadora de lis
-        const myLiElement = createLiNewElement (myResultImage, myResultName, myResultID);
 
-        //Ahora integro li dentro de ul y hago que se pueda clickar (result list)
+        const myLiElement = createLiNewElement (myResultImage, myResultName, myResultID);
         resultList.appendChild(myLiElement);
-        myLiElement.addEventListener('click', handlerClickFavorite)
+
+        myLiElement.addEventListener('click', handlerClickFavorite);
       }
     });
 }
 
-//hago funcion que cree un elemento li a partir de los parametros src, alt y name
 function createLiNewElement (src, alt, resultID) {
-  // Ahora creo el elemento nuevo li con DOM avanzado
   const myLiElement = document.createElement('li');
   myLiElement.classList.add('result__list__element');
   myLiElement.setAttribute('show-id', resultID);
 
-  // Ahora añado el elemento img al li
   const myImageNewElement = document.createElement('img');
   myImageNewElement.src = src;
   myImageNewElement.alt = alt;
   myImageNewElement.classList.add('result__image');
-  //Ahora integro img dentro de li
   myLiElement.appendChild(myImageNewElement);
-  // Ahora añado el elemento h2 que será el nombre de la serie al li
+
   const myTitleNewElement = document.createElement('h2');
   myTitleNewElement.classList.add('result__name');
-  //escribo el contenido de mi h2
   const newTitleElementContent = document.createTextNode(alt);
-  //integro el contenido de h2 en el elemento h2
   myTitleNewElement.appendChild(newTitleElementContent);
-
-  // Integro h2 al li
   myLiElement.appendChild(myTitleNewElement);
 
   return myLiElement;
 }
 
+
 function handlerClickFavorite() {
   let liShowSelected = event.currentTarget;
   liShowSelected.classList.add('select-User-Fav');
   let showID = event.currentTarget.getAttribute('show-id');
-  const favoriteObject =
-  { id: showID,
+  const favoriteObject = {
+    id: showID,
     src: liShowSelected.firstChild.src,
     title: liShowSelected.lastChild.innerHTML,
   };
 
-  //ejecucion:cuando user clickea en un li, solo se hará push en el array si ese li no es favorito, y vuelvo a pintar
+  //Sólo se hará push en el array si ese li no es favorito
   const isFavorite = showFavExist(showID);
   if(!isFavorite) {
     arrFavorites.push(favoriteObject);
@@ -99,7 +91,8 @@ function handlerClickFavorite() {
   }
 }
 
-// declaracion: si mi lista contiene un id igual, entonces existe y eso es true-->voy a suponer que apriori no lo tiene
+
+// Compruebo si mi lista contiene el id que paso por parámetro (supondremos que apriori no lo tiene)
 function showFavExist (showID) {
   let exist = false;
   for (let i=0; i<arrFavorites.length; i++) {
@@ -110,8 +103,10 @@ function showFavExist (showID) {
   return exist;
 }
 
+
 function printFavoriteArray () {
   myFavoriteList.innerHTML = '';
+
   for (let i=0; i<arrFavorites.length; i++) {
     myFavoriteList.innerHTML +=
     `<li data-show-id="${arrFavorites[i].id}" class="favoriteElement">
@@ -120,32 +115,34 @@ function printFavoriteArray () {
         <h2 class="favorite__name">${arrFavorites[i].title}</h2>
     </li>`;
   }
-  //cogeré todos los botones X para ponerles listener
+
   const allRemoveButton = document.querySelectorAll('.remove-fav-element');
   for (let i=0; i<arrFavorites.length; i++) {
     allRemoveButton[i].addEventListener('click', handleRemoveFavorite);
   }
 }
 
+
 function handleRemoveFavorite () {
   const favElementToRemove = event.currentTarget;
   const liShowID = favElementToRemove.parentElement.getAttribute('data-show-id');
-  console.log(liShowID);
   for (let i=0; i<arrFavorites.length; i++) {
     if (liShowID === arrFavorites[i].id) {
       arrFavorites.splice(i,1);
       printFavoriteArray();
-      //cuando ejecuto esta funcion, se deberia quitar el elemento tambien de local storage, entonces envío el array actualizado a esta funcionalidad de poder eliminar un fav
       localStorage.setItem('user-favorites-shows', JSON.stringify(arrFavorites));
     }
   }
 }
+
 
 function handleResetButton () {
   arrFavorites = [];
   printFavoriteArray();
   localStorage.removeItem('user-favorites-shows');
 }
+
+
 //listeners
 searchButton.addEventListener('click', handleButtonClick);
 resetButton.addEventListener('click', handleResetButton);
